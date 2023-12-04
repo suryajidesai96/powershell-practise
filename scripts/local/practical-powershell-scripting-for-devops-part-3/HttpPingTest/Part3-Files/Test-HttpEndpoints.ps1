7fe3996d3b47552381a52de00cfbe2deec80912a
suryajidesai96 <116329232+suryajidesai96@users.noreply.github.com>
2023-12-04 09:37:18 +0530

    Update Test-HttpEndpoints.ps1

--- scripts/local/practical-powershell-scripting-for-devops-part-3/HttpPingTest/Part3-Files/Test-HttpEndpoints.ps1
+++ scripts/local/practical-powershell-scripting-for-devops-part-3/HttpPingTest/Part3-Files/Test-HttpEndpoints.ps1
@@ -1,36 +1,79 @@
-[CmdletBinding()]
-param (
-    [Parameter(ValueFromPipeline = $true)]
-    [string]
-    $TestsFilePath =  '.\Tests.json'
-)
+#!/bin/bash
 
-#Lets Create a StopWatch Object.
-$Stopwatch = [System.Diagnostics.Stopwatch]::new()
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
 
-# Now Start the timer.
-$Stopwatch.Start()
+    case $key in
+        -TestsFilePath)
+        TestsFilePath="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)
+        # unknown option
+        shift
+        ;;
+    esac
+done
 
-# Convert JSON Config Files String value to a PowerShell Object
-$TestsObj = Get-Content -Path $TestsFilePath | ConvertFrom-Json
+# Set default value if TestsFilePath is not provided
+TestsFilePath=${TestsFilePath:-"./Tests.json"}
+
+# Create a Stopwatch Object
+start_time=$(date +%s.%N)
+
+# Convert JSON Config Files String value to a JSON object
+TestsObj=$(jq '.' "$TestsFilePath")
 
 # Import the Tester Function
-. ./lib/New-HttpTestResult.ps1
+source ./lib/New-HttpTestResult.sh
 
-$funcDef = ${function:New-HttpTestResult}.ToString()
+# Define the New-HttpTestResult function
+New_HttpTestResult() {
+    url=$1
+    name=$2
+    MaxRetryNo=${3:-10}  # Default to 10 if not provided
+    WaitTimeInSeconds=${4:-1}  # Default to 1 second if not provided
 
-$jobs = $TestsObj | ForEach-Object -Parallel {
+    Method="GET"
+    TestCounter=0
 
-    ${function:New-HttpTestResult} = $using:funcDef
+    while [ $TestCounter -lt $MaxRetryNo ]; do
+        ((TestCounter++))
 
-    New-HttpTestResult -TestArgs $_
-} -AsJob -ThrottleLimit 50
+        start_time=$(date +%s%N)
+        response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+        end_time=$(date +%s%N)
 
-$jobs | Receive-Job -Wait | Format-Table
+        duration=$((($end_time - $start_time) / 1000000))
 
-# Now Stop the timer.
-$Stopwatch.Stop()
+        if [ "$response" -eq 200 ]; then
+            break
+        else
+            sleep $WaitTimeInSeconds
+        fi
+    done
 
-$TestDuration  =  $Stopwatch.Elapsed.TotalSeconds
+    timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-Write-Host "Total Script Execution Time: $($TestDuration) Seconds"
+    result="{\"name\":\"$name\",\"status_code\":\"$response\",\"status_description\":\"\",\"attempt_no\":\"$TestCounter/$MaxRetryNo\",\"responsetime_ms\":\"$duration\",\"timestamp\":\"$timestamp\"}"
+
+    echo "$result"
+}
+
+# Define the function code
+funcDef=$(declare -f New_HttpTestResult)
+
+# Execute parallel jobs
+results=$(echo "$TestsObj" | parallel --jobs 50 --halt now,fail=1 "eval \"$funcDef\"; New_HttpTestResult {}")
+
+echo "$results" | column -t
+
+# Stop the Stopwatch
+end_time=$(date +%s.%N)
+
+# Calculate the elapsed time
+elapsed_time=$(echo "$end_time - $start_time" | bc)
+
+echo "Total Script Execution Time: $elapsed_time Seconds"
