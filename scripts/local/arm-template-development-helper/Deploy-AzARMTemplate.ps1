f6f419b6213f0d8f82f41e6c24b5ba72691564db
suryajidesai96 <116329232+suryajidesai96@users.noreply.github.com>
2023-12-04 10:00:43 +0530

    Update Deploy-AzARMTemplate.ps1

--- scripts/local/arm-template-development-helper/Deploy-AzARMTemplate.ps1
+++ scripts/local/arm-template-development-helper/Deploy-AzARMTemplate.ps1
@@ -1,132 +1,87 @@
-<#
-    .DESCRIPTION
-    Tests, Validates or Deploys Azure Resource Manager Template
+#!/bin/bash
 
-    .PARAMETER Test
-    Switch for only running the Offline Template Validation
+# Configuration
+OperationsPath="$(dirname "$(readlink -f "$0")")"
+ArmArtifactsPath="$OperationsPath/infrastructure"
 
-    .PARAMETER WhatIf
-    Switch for only running the What-If Validation, that simulates a deployment via the Azure ARM REST API
+# Azure Environment Configuration
+TenantId='#{YOUR-AZUREAD-TENANT-ID}#'
+SubscriptionId='#{YOUR-AZURE-SUBSCRIPTION-ID}#'
 
-    .PARAMETER Force
-    Switch for skipping the Confirmation Prompt for Deployment
-    .NOTES
-    MIT License
+# Connect to the Correct Tenant and Subscription
+CurrentContext=$(az account show --query "{SubscriptionId:id,TenantId:tenantId}" --output json)
+if [ -z "$CurrentContext" ]; then
+    az login --service-principal -u "$TenantId" -p "$SubscriptionId" --tenant "$TenantId"
+fi
 
-    Copyright (c) 2020 Mert Senel
+# Service Constants
+ProjectName='mert'
+Stage='dev'
+ServiceType='fnc'
+Region='wus'
 
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+# Service Curated Parameters
+ResourceGroupName="${ProjectName}${Stage}${ServiceType}${Region}01"
+TemplateFile="$ArmArtifactsPath/template.json"
+TemplateParameterFile="$ArmArtifactsPath/parameters.${Stage}.${Region}01.json"
 
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
+# Deployment Helpers
+export ErrorActionPreference="Stop"
+if [ "$Test" = true ]; then
+    az deployment group validate \
+        --resource-group "$ResourceGroupName" \
+        --template-file "$TemplateFile" \
+        --parameters "@$TemplateParameterFile"
+    exit $?
+fi
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-#>
-#Requires -Module @{ ModuleName = 'Az'; ModuleVersion = '4.2' }
+if [ "$WhatIf" = true ]; then
+    az deployment group what-if \
+        --resource-group "$ResourceGroupName" \
+        --template-file "$TemplateFile" \
+        --parameters "@$TemplateParameterFile"
+    exit $?
+fi
 
-[CmdletBinding()]
-param (
-    [Parameter()][switch]$WhatIf,
-    [Parameter()][switch]$Test,
-    [Parameter()][switch]$Force
-)
-#region Configuration
-#Deployment Assets Configuration
-# Go Up one level so you can pick a project
-# This is here in case you want to keep your script in different folder.
-$OperationsPath = Split-Path $PSScriptRoot 
-$ArmArtifactsPath = "$OperationsPath\infrastructure" # Target the folder which desired ARM Template and Parameter files are in. 
+# Deployment ("Validate & Deploy" or "Forced" Deployment)
+echo "Deploying $Region"
+DeploymentName=$(uuidgen)
+az deployment group create \
+    --name "$DeploymentName" \
+    --resource-group "$ResourceGroupName" \
+    --template-file "$TemplateFile" \
+    --parameters "@$TemplateParameterFile" \
+    --mode Incremental \
+    --verbose
 
-#Azure Environment Configuration
-$TenantId = '#{YOUR-AZUREAD-TENANT-ID}#'
-$SubscriptionId = '#{YOUR-AZURE-SUBSCRIPTION-ID}#'
-#endregion
+DeploymentProvisioningState=$(az deployment group show \
+    --name "$DeploymentName" \
+    --resource-group "$ResourceGroupName" \
+    --query "provisioningState" \
+    --output tsv)
 
-#region Connect to Correct Tenant and Subscription
-$CurrentContext = Get-AzContext
-#If there is no AzContext found connect to desired Subscription and Tenant
-if (!$CurrentContext) {
-    Connect-AzAccount -Tenant $TenantId -Subscription $SubscriptionId -UseDeviceAuthentication
-    $CurrentContext = Get-AzContext
-}
-#If subscription ID doesnt match, call the Set-AzContext with 
-#SubscriptionId and TenantId to allow switch between tenants as well. 
-if ($CurrentContext.Subscription.Id -ne $SubscriptionId) {
-    $CurrentContext = Set-AzContext -Subscription $SubscriptionId -Tenant $TenantId
-}
-#endregion
+# Tell me if my Deployment was Successful
+if [ "$DeploymentProvisioningState" = "Succeeded" ]; then
+    echo "Deployed $Region Successfully"
+    exit 0
+fi
 
-#region Service Constants
-$ProjectName = 'mert'
-$Stage = 'dev'
-$ServiceType = 'fnc'
-$Region = "wus"
-#endregion
+# Or if there was an error during Deployment, I want to know about it
+if [ "$DeploymentProvisioningState" != "Succeeded" ]; then
+    CorrelationId=$(az deployment group show \
+        --name "$DeploymentName" \
+        --resource-group "$ResourceGroupName" \
+        --query "correlationId" \
+        --output tsv)
 
-#region Service Curated Parameters
-$ResourceGroupName = $ProjectName + $Stage + $ServiceType + $Region + '01'
-$TemplateFile = "$ArmArtifactsPath\template.json"
-$TemplateParameterFile = "$ArmArtifactsPath\parameters.$Stage.$($Region)01.json"
+    az monitor activity-log list \
+        --correlation-id "$CorrelationId" \
+        --query "[].{Message:properties.statusMessage}" \
+        --output table
 
-$ARGS = @{
-    ResourceGroupName     = $ResourceGroupName
-    TemplateFile          = $TemplateFile
-    TemplateParameterFile = $TemplateParameterFile
-    Mode                  = 'Incremental'
-    Verbose               = $true
-    ErrorAction           = 'Stop'
-}
-#endregion
+    exit 1
+fi
 
-#region Deployment Helpers
-$ErrorActionPreference = "Stop"
-if ($Test) {
-    Test-AzResourceGroupDeployment @ARGS
-    exit $LASTEXITCODE
-}
-if ($WhatIf) {
-    $WhatIfResult = Get-AzResourceGroupDeploymentWhatIfResult @ARGS `
-                                    -ResultFormat FullResourcePayloads
-    $WhatIfResult
-    exit $LASTEXITCODE
-}
-#endregion
-
-#region Deployment ("Validate & Deploy" or "Forced" Deployment)
-try {
-    $PromptForConfirmation = (($Force) ? $false : $true)
-    Write-Host "Deploying $Region"
-    $Deployment = New-AzResourceGroupDeployment @ARGS `
-                                                -Name "$(New-Guid)" `
-                                                -Confirm:$PromptForConfirmation `
-                                                -WhatIfResultFormat FullResourcePayloads
-}
-catch {
-    Write-Host $_.Exception.Message
-    exit $LASTEXITCODE
-}
-
-# Tell me If my Deployment was Successfull
-if ($Deployment.ProvisioningState -eq "Succeeded") {
-    Write-Host "Deployed $Region Successfully"
-}
-# Or if there was an error during Deployment I want to know about it
-elseif ($Deployment.ProvisioningState -ne "Succeeded" -and ($Deployment.CorrelationId)) {
-    Write-Host "$(Get-AzLog -CorrelationId $Deployment.CorrelationId)"
-}
 # Else just let me know if there was no AzResourceGroupDeployment Object found
-elseif (!($Deployment)) {
-    Write-Warning "No AzResourceGroupDeployment Object Found"
-}
-#endregion
+echo "No AzResourceGroupDeployment Object Found"
+exit 1
