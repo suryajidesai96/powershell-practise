3b8b3106972b65f714cf65e02ccbbc14d8a3b5e3
suryajidesai96 <116329232+suryajidesai96@users.noreply.github.com>
2023-12-04 09:31:00 +0530

    Update New-HttpTestResult.ps1

--- scripts/local/practical-powershell-scripting-for-devops-part-3/HttpPingTest/Part1-Files/lib/New-HttpTestResult.ps1
+++ scripts/local/practical-powershell-scripting-for-devops-part-3/HttpPingTest/Part1-Files/lib/New-HttpTestResult.ps1
@@ -1,24 +1,29 @@
-function New-HttpTestResult {
-    param (
-        [Parameter(ValueFromPipeline=$true)]
-        [PSCustomObject]
-        $TestArgs
-    )
-    $ProgressPreference = 'SilentlyContinue'
+#!/bin/bash
 
-    $Method = 'Get'
+New_HttpTestResult() {
+    url=$1
+    name=$2
 
-    $duration = Measure-Command {
-        $Response = Invoke-WebRequest -Uri $TestArgs.url -Method $Method -SkipHttpErrorCheck
-    }
+    Method="GET"
 
-    $result = [PSCustomObject]@{
-        name = $TestArgs.name
-        status_code = $Response.StatusCode.ToString()
-        status_description = $Response.StatusDescription
-        responsetime_ms = $duration.Milliseconds
-        timestamp = (get-date).ToString('O')
-    }
+    start_time=$(date +%s%N)
+    response=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+    end_time=$(date +%s%N)
 
-    return $result
+    duration=$((($end_time - $start_time) / 1000000))
+
+    timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+    status_code=$(echo "$response" | tr -d '\n')
+
+    result="{\"name\":\"$name\",\"status_code\":\"$status_code\",\"status_description\":\"\",\"responsetime_ms\":\"$duration\",\"timestamp\":\"$timestamp\"}"
+
+    echo "$result"
 }
+
+# Example usage:
+url="https://example.com"
+name="TestName"
+result=$(New_HttpTestResult "$url" "$name")
+
+echo "Result: $result"
