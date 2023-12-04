0e74e16aff2764ca9a8ea7f68c5c6a8f2e212208
suryajidesai96 <116329232+suryajidesai96@users.noreply.github.com>
2023-12-04 09:29:33 +0530

    Update Test-HttpEndpoints.ps1

--- scripts/local/practical-powershell-scripting-for-devops-part-2/HttpPingTest/Part2-Files/Test-HttpEndpoints.ps1
+++ scripts/local/practical-powershell-scripting-for-devops-part-2/HttpPingTest/Part2-Files/Test-HttpEndpoints.ps1
@@ -1,19 +1,35 @@
-[CmdletBinding()]
-param (
-    [Parameter(ValueFromPipeline = $true)]
-    [string]
-    $TestsFilePath =  '.\Tests.json'
-)
+#!/bin/bash
 
-# Convert JSON Config Files String value to a PowerShell Object
-$TestsObj = Get-Content -Path $TestsFilePath | ConvertFrom-Json
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+        -TestsFilePath)
+        TestsFilePath="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)
+        # unknown option
+        shift
+        ;;
+    esac
+done
+
+# Set default value if TestsFilePath is not provided
+TestsFilePath=${TestsFilePath:-"./Tests.json"}
+
+# Convert JSON Config Files String value to a JSON object
+TestsObj=$(jq '.' "$TestsFilePath")
 
 # Import the Tester Function
-. ./lib/New-HttpTestResult.ps1
+source ./lib/New-HttpTestResult.sh
 
 # Loop through Test Objects and get the results as a collection
-$TestResults = foreach ($Test in $TestsObj) { 
-    New-HttpTestResult -TestArgs $Test 
-}
+TestResults=""
+while IFS= read -r Test; do
+    TestResults+="$(New_HttpTestResult "$Test")"$'\n'
+done <<< "$TestsObj"
 
-$TestResults | Format-Table -AutoSize
+echo "$TestResults" | column -t
