90f38a28fb3eab1fa3b932017dff25e58402556d
suryajidesai96 <116329232+suryajidesai96@users.noreply.github.com>
2023-12-04 09:38:33 +0530

    Update Test-SemVerWithRegex.ps1

--- scripts/local/practical-powershell-scripting-for-devops-part-4/SemverControl/Part4/Test-SemVerWithRegex.ps1
+++ scripts/local/practical-powershell-scripting-for-devops-part-4/SemverControl/Part4/Test-SemVerWithRegex.ps1
@@ -1,27 +1,27 @@
-$refsInputArray = @("refs/heads/releases/1.0.0",
-                    "refs/heads/releases/1.a.0",
-                    "refs/heads/releases/1.2.0.5.6.9",
-                    "refs/heads/release/1.1.1",
-                    "refs/heads/releases/1.0.1",
-                    "refs/heads/releases/2.4.3",
-                    "refs/heads/main",
-                    "refs/heads/develop")
+#!/bin/bash
 
-#https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-$semVerRegex = '^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
+refsInputArray=("refs/heads/releases/1.0.0"
+                "refs/heads/releases/1.a.0"
+                "refs/heads/releases/1.2.0.5.6.9"
+                "refs/heads/release/1.1.1"
+                "refs/heads/releases/1.0.1"
+                "refs/heads/releases/2.4.3"
+                "refs/heads/main"
+                "refs/heads/develop")
 
-$refsInputArray | ForEach-Object {
-    $refParts = ($PSItem -split '/')
+# https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+semVerRegex='^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$'
 
-    if ($refParts[-2] -eq 'releases') {
-        if ("$($refParts[-1])" -match $semVerRegex) {
-            Write-Host "Reference $($PSItem) is in releases folder and has a correct semver format"
-        }
-        else {
-            Write-Error "Reference $($PSItem) is in releases folder but has an incorrect semver format"
-        }
-    }
-    else {
-        Write-Error "Reference $($PSItem) is not in releases folder"
-    }
-}
+for ref in "${refsInputArray[@]}"; do
+    IFS='/' read -ra refParts <<< "$ref"
+
+    if [ "${refParts[-2]}" == "releases" ]; then
+        if [[ "${refParts[-1]}" =~ $semVerRegex ]]; then
+            echo "Reference $ref is in releases folder and has a correct semver format"
+        else
+            echo "Reference $ref is in releases folder but has an incorrect semver format"
+        fi
+    else
+        echo "Reference $ref is not in releases folder"
+    fi
+done
